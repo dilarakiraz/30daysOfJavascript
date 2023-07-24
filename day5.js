719ebebbc95218570a8e668b7b9a392a4387b4d5
dilarakiraz <dilarakiraz12345@gmail.com>
2023-07-24 15:27:02 +0300

    Array

---
+++ day5.js
@@ -0,0 +1,15 @@
+//let names = Array()
+
+//console.log(names)
+
+//let names2 = ['Dilara',22,2000,'Kiraz']
+
+//console.log(names2.length) 
+let array1=[1,2,3]
+let array2=[4,5,6]
+
+let result= array1.concat(array2)
+console.log(result)
+
+let names=['Tayfun','Mehmet','Ahmet']
+console.log(names.indexOf('Ahmet'))
