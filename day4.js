e9658a430ecd40d7af7e9460b2926b1528f51bb3
dilarakiraz <dilarakiraz12345@gmail.com>
2023-07-24 14:56:59 +0300

    if-else

---
+++ day4.js
@@ -0,0 +1,38 @@
+let result = parseInt('2 + 5 kaç yapar', '2 + 5')
+
+if(result == 7){
+    console.log('Doğru cevabı verdin')
+
+}else{
+    console.log('yanlış cevap!')
+}
+
+console.log(
+
+    result == 7 ?'DOĞRU CEVAP' : 'YANLIŞ CEVAP'
+)
+
+switch(result){
+    case 7: 
+    console.log('Doğru cevap - switch')
+    break
+
+    case 5:
+        console.log('hatalı cevap')
+        break
+
+        default:
+            console.log('hatalı cevap - switch')
+}
+
+
+let number = 5
+if(number == 4){
+
+}else if(number >= 5){
+
+}else if(number > 5){
+
+}else{
+
+}
