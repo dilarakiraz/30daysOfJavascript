5d9b5215265853bf1e14b7ebc8e7d787b5cc0722
dilarakiraz <dilarakiraz12345@gmail.com>
2023-07-24 09:21:53 +0300

    create,update

---
+++ introduction.js
@@ -0,0 +1,12 @@
+console.log(typeof 'NKU')
+console.log(typeof 5)
+console.log(typeof true)
+console.log(typeof null)
+console.log(typeof undefined) 
+
+let youtubeChannel = 'prototurk'
+let channelAge = 110
+let isChannelStillActive = true
+let secondChannel = null
+let channelMoney = undefined
+console.log(youtubeChannel)
